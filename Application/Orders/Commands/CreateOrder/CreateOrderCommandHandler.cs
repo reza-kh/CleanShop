5ea7bc971fd3bc5d550eb5a29d16cdad58384dfe
reza-kh <khosrawireza@gmail.com>
2025-08-17 00:21:10 +0330

    added decrease form inventory when order created

--- Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -12,12 +12,14 @@ namespace Application.Orders.Commands.CreateOrder
     public sealed class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Guid>
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IInventoryRepository _inventoryRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public CreateOrderCommandHandler(IOrderRepository orderRepository, IUnitOfWork unitOfWork)
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IUnitOfWork unitOfWork, IInventoryRepository inventoryRepository)
         {
             _orderRepository = orderRepository;
             _unitOfWork = unitOfWork;
+            _inventoryRepository = inventoryRepository;
         }
 
         public async Task<Guid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
@@ -25,7 +27,15 @@ namespace Application.Orders.Commands.CreateOrder
             var orderItems = request.Items
                 .Select(i => new OrderItem(i.ProductId, i.Quantity, i.UnitPrice, request.CreatorUserId))
                 .ToList();
-
+            foreach (var item in orderItems) 
+            { 
+                var inventory =await _inventoryRepository.GetByProductIdAsync(item.ProductId,cancellationToken);
+                if (inventory == null)
+                {
+                    return Guid.Empty;//TODO Handle Not Created Inventory
+                }
+                inventory?.Decrease(item.Quantity);
+            }
             var order =Order.Create(request.CustomerId, orderItems,request.CreatorUserId);
             
 
