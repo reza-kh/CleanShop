574f053480c2773c158b5e0894fda7d57b8d75a7
reza-kh <khosrawireza@gmail.com>
2025-08-16 22:10:24 +0330

    add config for preparing for db migration

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,8 @@
+using Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
