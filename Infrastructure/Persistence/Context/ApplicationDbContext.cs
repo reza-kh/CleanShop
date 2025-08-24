4aacc4635a192f04789826f09ee12d5d94ea482f
reza-kh <khosrawireza@gmial.com>
2025-08-24 15:59:17 +0330

    implemented IApplicationDbContext

--- Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -13,19 +13,64 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence.Context;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
 
-    public DbSet<Customer> Customers => Set<Customer>();
-    public DbSet<Product> Products => Set<Product>();
-    public DbSet<Order> Orders => Set<Order>();
-    public DbSet<InventoryItem> InventoryItems => Set<InventoryItem>();
+    private readonly ILogger<ApplicationDbContext> _logger;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ILogger<ApplicationDbContext> logger)
+        : base(options)
+    {
+        _logger = logger;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    async Task<OperationResult> IApplicationDbContext.SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var affected = await base.SaveChangesAsync(cancellationToken);
+
+            if (affected > 0)
+                return OperationResult.Success(new List<string> { ApplicationMessages.OperationSuccesfulDone }, affected);
+
+            if (affected == 0)
+                return OperationResult.NoChanges(new List<string> { ApplicationMessages.NoChangesDetected }, affected);
+
+            return OperationResult.Failure(new List<string> { ApplicationMessages.OperationFailureDone }, affected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "DataBase_Exception");
+            return OperationResult.Failure(new List<string> { ApplicationMessages.OperationFailureDone });
+        }
+    }
+
+    OperationResult IApplicationDbContext.SaveChanges()
+    {
+        try
+        {
+            var affected =  base.SaveChanges();
+
+            if (affected > 0)
+                return OperationResult.Success(new List<string> { ApplicationMessages.OperationSuccesfulDone }, affected);
+
+            if (affected == 0)
+                return OperationResult.NoChanges(new List<string> { ApplicationMessages.NoChangesDetected }, affected);
+
+            return OperationResult.Failure(new List<string> { ApplicationMessages.OperationFailureDone }, affected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "DataBase_Exception");
+            return OperationResult.Failure(new List<string> { ApplicationMessages.OperationFailureDone });
+        }
     }
 }
